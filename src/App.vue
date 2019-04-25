6f06f6f07134a055e2c3ca5d8a6bec216bf68d82
Carlos Lantigua <clantigua954@gmail.com>
2019-04-25 10:39:31 -0400

    completed base todo application

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos"/>
+    <Header v-bind:header="header"/>
+    <Todos v-bind:todos="todos" @del-todo="deleteTodo"/>
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
-
+import Header from "./components/Header";
 export default {
   name: "app",
   components: {
-    Todos
+    Todos,
+    Header
   },
   // state
   data() {
@@ -24,15 +26,21 @@ export default {
         {
           id: 2,
           title: "Todo Two",
-          completed: false
+          completed: true
         },
         {
           id: 3,
           title: "Todo Three",
           completed: false
         }
-      ]
+      ],
+      header: "Todo App"
     };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(item => item.id !== id);
+    }
   }
 };
 </script>
@@ -47,5 +55,10 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+#app {
+  max-width: 500px;
+  width: 500px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>{{header}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["header"]
+};
+</script>
+
+<style scoped>
+</style>
+
+
